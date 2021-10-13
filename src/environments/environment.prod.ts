cdce5503f5ddcc3667e6a19e17e4ab59d7445fa6
Nuno Caetano <a21902813@alunos.ulht.pt>
2021-10-13 20:45:09 +0100

    google social

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://18.117.62.47:8000',
   aws_client_id: '1a0rhvcdiebbbpqbg5domh24r5',
-  redirect_uri: 'http://18.117.62.47:8000',
+  redirect_uri: 'http://18.224.189.162/home_tab',
 };
